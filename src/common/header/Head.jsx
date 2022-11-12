0ef31bd7a4eccb492ba44cf2d533004d1128e719
Burak GUL <burakgl2465@gmail.com>
2022-11-12 21:05:13 +0300

    Overall design is complete.

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> 0424 248 28 94</label>
+            <i className='fa fa-envelope'></i>
+            <label> deepohakkinda.com.tr
+</label>
+          </div>
+          <div className='right row RText'>
+            <label>Tema SSS</label>
+            <label>Yardım?</label>
+            <span>🏳️</span>
+            <label>TUR</label>
+            <span>🏳️</span>
+            <label>EN</label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
